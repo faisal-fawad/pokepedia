423b684e242906378a22f960ba64d440b801d238
Faisal Fawad <faisalfawad2004@gmail.com>
2023-12-31 18:35:26 -0500

    Redis bug fix

--- src/lib/server/redis.js
+++ src/lib/server/redis.js
@@ -2,5 +2,15 @@ import { Redis } from 'ioredis';
 import { REDIS_URI } from '$env/static/private';
 
 let r;
-if (REDIS_URI) r = new Redis(REDIS_URI);
+if (REDIS_URI) {
+    try {
+        r = new Redis(REDIS_URI);
+        const info = await r.info();
+        console.log("Connected to Redis successfully");
+    } catch (e) {
+        r.disconnect();
+        r = null;
+        console.log("Failed to connect to Redis");
+    }
+}
 export const redis = r;
