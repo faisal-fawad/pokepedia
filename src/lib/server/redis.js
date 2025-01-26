84046e2a23e5d425d06f20c83873ba146a05ef70
Faisal Fawad <faisalfawad2004@gmail.com>
2025-01-25 23:28:18 -0500

    Remove Redis; discontinued

--- src/lib/server/redis.js
+++ src/lib/server/redis.js
@@ -1,16 +1,16 @@
-import { Redis } from 'ioredis';
-import { REDIS_URI } from '$env/static/private';
+// import { Redis } from 'ioredis';
+// import { REDIS_URI } from '$env/static/private';
 
-let r;
-if (REDIS_URI) {
-    try {
-        r = new Redis(REDIS_URI);
-        const info = await r.info();
-        console.log("Connected to Redis successfully");
-    } catch (e) {
-        r.disconnect();
-        r = null;
-        console.log("Failed to connect to Redis");
-    }
-}
-export const redis = r;
+// let r;
+// if (REDIS_URI) {
+//     try {
+//         r = new Redis(REDIS_URI);
+//         const info = await r.info();
+//         console.log("Connected to Redis successfully");
+//     } catch (e) {
+//         r.disconnect();
+//         r = null;
+//         console.log("Failed to connect to Redis");
+//     }
+// }
+export const redis = null;
